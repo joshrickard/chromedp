1a1c8690a6bc8b3ab949891b0c67f61e4bd4f5d3
Josh Rickard <josh.rickard@gmail.com>
2020-08-04 12:17:21 -0500

    Avoid setting Pdeathsig in restricted environments

--- allocate_linux.go
+++ allocate_linux.go
@@ -4,13 +4,33 @@ package chromedp
 
 import (
 	"os/exec"
+	"strings"
 	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
 func allocateCmdOptions(cmd *exec.Cmd) {
+	// In restricted environments (e.g. AWS Lambda), we are unable to set Pdeathsig
+	if isPdeathsigRestricted() {
+		return
+	}
+
 	if cmd.SysProcAttr == nil {
 		cmd.SysProcAttr = new(syscall.SysProcAttr)
 	}
 	// When the parent process dies (Go), kill the child as well.
 	cmd.SysProcAttr.Pdeathsig = syscall.SIGKILL
+}
+
+func isPdeathsigRestricted() bool {
+	var sig int
+
+	err := unix.Prctl(unix.PR_GET_PDEATHSIG, uintptr(unsafe.Pointer(&sig)), 0, 0, 0)
+	if err != nil {
+		return strings.Contains(err.Error(), "operation not permitted")
+	}
+
+	return false
 }
